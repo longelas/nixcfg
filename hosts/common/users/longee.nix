ec758e803a770e63500c75bd6e14f017ced4921e
longelas <mt.hrvth@gmail.com>
2025-01-09 17:00:37 +0100

    Update longee.nix

--- hosts/common/users/longee.nix
+++ hosts/common/users/longee.nix
@@ -22,7 +22,7 @@
       "docker"
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGsUQTVZR2GONHiHZEKH2bsomxbcpnzF9+oj9Qk/TXJ+ mt.hrvth@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGsUQTVZR2GONHiHZEKH2bsomxbcpnzF9+oj9Qk/TXJ+ mt.hrvth@gmail.com"];
     packages = [inputs.home-manager.packages.${pkgs.system}.default];
   };
   home-manager.users.longee =
