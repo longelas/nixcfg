f068f1bb9af54134a35bf3e53493b454ee7575e7
longelas <mt.hrvth@gmail.com>
2025-01-09 18:07:21 +0100

    a

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -2,12 +2,10 @@
   imports = [
     ./audio.nix
     ./bluetooth.nix
-    ./boot.nix
     ./env.nix
     ./home-manager.nix
     ./hyprland.nix
     ./mime.nix
-    ./nh.nix
     ./timezone.nix
     ./zram.nix
   ];
