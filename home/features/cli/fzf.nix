581a5fd05a3267200cbed092ffbd108598e25ca0
longelas <mt.hrvth@gmail.com>
2025-01-22 16:23:26 +0100

    Update fzf.nix

--- home/features/cli/fzf.nix
+++ home/features/cli/fzf.nix
@@ -9,7 +9,7 @@ with lib; let
     in{
         options.features.cli.fzf.enable = mkEnableOption "enable fuzzy finder";
 
-        config = mkIf cfg.enable = {
+        config = mkIf cfg.enable {
             programs.fzf = {
                 enable = true;
                 enableFishIntegration = true;
