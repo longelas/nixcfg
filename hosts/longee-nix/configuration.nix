7eca736e54511c54db78fe4e5de31289bc74e424
longelas <mt.hrvth@gmail.com>
2025-01-23 18:06:31 +0100

    Update configuration.nix

--- hosts/longee-nix/configuration.nix
+++ hosts/longee-nix/configuration.nix
@@ -96,4 +96,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
 }
