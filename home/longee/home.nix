447b11191090984577d9f35ed6e1cf976f8b688a
longelas <mt.hrvth@gmail.com>
2025-01-23 19:47:48 +0100

    Update home.nix

--- home/longee/home.nix
+++ home/longee/home.nix
@@ -83,10 +83,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  programs.home-manager.sharedModules = [
-    {
-      stylix.enable = true;
-      }
-  ];
 }
