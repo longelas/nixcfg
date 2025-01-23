787b776c36ad79784d3e9eaa24ef3093f444eff0
longelas <mt.hrvth@gmail.com>
2025-01-23 20:06:18 +0100

    Update configuration.nix

--- hosts/longee-nix/configuration.nix
+++ hosts/longee-nix/configuration.nix
@@ -67,7 +67,27 @@
     enable = true;
     polarity = "dark";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-    base16Scheme = "${inputs.tt-schemes}/base16/gruvbox-material-dark-medium.yaml";
+    base16Scheme = 
+    ""
+    scheme: "Gruvbox Material Dark, Medium"
+    author: "Mayush Kumar (https://github.com/MayushKumar), sainnhe (https://github.com/sainnhe/gruvbox-material-vscode)"
+    base00: "292828"
+    base01: "32302f"
+    base02: "504945"
+    base03: "665c54"
+    base04: "bdae93"
+    base05: "ddc7a1"
+    base06: "ebdbb2"
+    base07: "fbf1c7"
+    base08: "ea6962"
+    base09: "e78a4e"
+    base0A: "d8a657"
+    base0B: "a9b665"
+    base0C: "89b482"
+    base0D: "7daea3"
+    base0E: "d3869b"
+    base0F: "bd6f3e"
+    "";
 
 
     fonts = {
