3362b63b7aa46c4912c4367ae0f4ef94c1cb4c8f
longelas <mt.hrvth@gmail.com>
2025-01-24 14:05:48 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix/release-24.05";
+      # url = "github:danth/stylix/release-24.05";
+      url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
