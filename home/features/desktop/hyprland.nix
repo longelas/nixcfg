ca58a3ecc0da485cdb371f8489051e4d04662f85
longelas <mt.hrvth@gmail.com>
2025-01-24 12:40:47 +0100

    Update hyprland.nix

--- home/features/desktop/hyprland.nix
+++ home/features/desktop/hyprland.nix
@@ -49,14 +49,14 @@ in {
                         sensitivity = 0;
                     };
 
-                    general = {
-                        gaps_in = 5;
-                        gaps_out = 5;
-                        border_size = 1;
-                        "col.active_border" = "rgba(9742b5ee) rgba(9742b5ee) 45deg";
-                        "col.inactive_border" = "rgba(595959aa)";
-                        layout = "dwindle";
-                    };
+                    # general = {
+                    #     gaps_in = 5;
+                    #     gaps_out = 5;
+                    #     border_size = 1;
+                    #     "col.active_border" = "rgba(9742b5ee) rgba(9742b5ee) 45deg";
+                    #     "col.inactive_border" = "rgba(595959aa)";
+                    #     layout = "dwindle";
+                    # };
 
                     # decoration = {
                     #     "col.shadow" = "rgba(1E202966)";
@@ -75,23 +75,23 @@ in {
                     #     inactive_opacity = 0.5;
                     # };
 
-                    animations = {
-                        enabled = true;
-                        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-                        animation = [
-                            "windows, 1, 7, myBezier"
-                            "windowsOut, 1, 7, default, popin 80%"
-                            "border, 1, 10, default"
-                            "borderangle, 1, 8, default"
-                            "fade, 1, 7, default"
-                            "workspaces, 1, 6, default"
-                        ];
-                    };
+                    # animations = {
+                    #     enabled = true;
+                    #     bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+                    #     animation = [
+                    #         "windows, 1, 7, myBezier"
+                    #         "windowsOut, 1, 7, default, popin 80%"
+                    #         "border, 1, 10, default"
+                    #         "borderangle, 1, 8, default"
+                    #         "fade, 1, 7, default"
+                    #         "workspaces, 1, 6, default"
+                    #     ];
+                    # };
 
-                    dwindle = {
-                        pseudotile = true;
-                        preserve_split = true;
-                    };
+                    # dwindle = {
+                    #     pseudotile = true;
+                    #     preserve_split = true;
+                    # };
 
                     # master.new_status = "master";
                     master.new_is_master = true;
